0973f1a6d3c97c5ed034f7f70b994f6c0de96e31
ßingen <bingentxu@gmail.com>
2018-06-19 14:04:59 +0200

    Skip interfaces in coverage

--- .solcover.js
+++ .solcover.js
@@ -3,4 +3,5 @@ module.exports = {
     // rsync is needed so symlinks are resolved on copy of lerna packages
     testCommand: 'rsync --copy-links -r ../node_modules/@aragon node_modules && node --max-old-space-size=4096 ../node_modules/.bin/truffle test --network coverage',
     copyNodeModules: true,
+    skipFiles: ['interfaces/IRegistry.sol', 'interfaces/IStaking.sol', 'interfaces/IVoting.sol'],
 }
